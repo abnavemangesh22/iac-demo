25c3710dc77155883c55d5ab7a98320b2c7610b0
Mangesh <mangesh@yahoo.com>
2023-09-25 23:22:02 +0530

    new-version

--- provider.tf
+++ provider.tf
@@ -3,15 +3,4 @@ provider "aws" {
 }
 
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.17.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "4.0.4"
-    }
-  }
-}
+
