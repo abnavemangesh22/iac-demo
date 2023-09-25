2ed97b8fa4b0a8c698beb35bd53990ebfbe127c7
abnavemangesh22 <69311384+abnavemangesh22@users.noreply.github.com>
2023-09-25 22:19:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,20 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+
+  }
+
+}
